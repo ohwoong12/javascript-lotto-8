9df66e552b206f9aeab66e51cf6dbf01067cc04c
OH WOONG KWON <oungtom1212@naver.com>
2025-11-02 01:03:41 +0900

    feat: 로또 생성하는 함수 추가
    별도의 함수로 로또 생성의 기능만 전담

---
+++ src/LottoGnerator.js
@@ -0,0 +1,17 @@
+import { Random } from '@woowacourse/mission-utils';
+
+/**
+ * 주어진 로또 구매 수량에 따라 로또 번호를 생성 후 반환하는 함수
+ * @param {number} purchaseCount - 로또 구매 수량
+ * @returns {string[]} - 최종 로또 용지
+ */
+export default function LottoGnerator(purchaseCount) {
+  const totalLottoTickets = [];
+
+  for (let i = 0; i < purchaseCount; i += 1) {
+    const newNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+    totalLottoTickets.push(newNumbers);
+  }
+
+  return totalLottoTickets;
+}

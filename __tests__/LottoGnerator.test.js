c8f393c3840c420c8d17c392c21f85bd888297dc
OH WOONG KWON <oungtom1212@naver.com>
2025-11-02 01:17:54 +0900

    test: 로또 생성 함수 테스트 코드 추가

---
+++ __tests__/LottoGnerator.test.js
@@ -0,0 +1,33 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import LottoGnerator from '../src/LottoGnerator.js';
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickUniqueNumbersInRange);
+};
+
+describe('로또 생성 함수 테스트', () => {
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  test('로또 생성 함수 테스트', () => {
+    const purchaseCount = 3;
+    const expectedLottos = [
+      [1, 2, 3, 4, 5, 6],
+      [7, 8, 9, 10, 11, 12],
+      [13, 14, 15, 16, 17, 18],
+    ];
+
+    mockRandoms(expectedLottos);
+
+    const result = LottoGnerator(purchaseCount);
+
+    expect(MissionUtils.Random.pickUniqueNumbersInRange).toHaveBeenCalledTimes(
+      purchaseCount,
+    );
+    expect(result).toEqual(expectedLottos);
+  });
+});

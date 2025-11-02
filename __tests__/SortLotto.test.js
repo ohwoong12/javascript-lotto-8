583e6ac7f5edb4254f30bff89c5bb2a3dde79d3a
OH WOONG KWON <oungtom1212@naver.com>
2025-11-02 23:09:23 +0900

    test: SortLotto 기능 변경으로 테스트 코드 변경

--- __tests__/SortLotto.test.js
+++ __tests__/SortLotto.test.js
@@ -1,13 +1,7 @@
-import sortLotto from '../src/SortLotto.js';
+import sortLotto from '../src/utils/SortLotto.js';
 
-test('SortLotto: 정렬되지 않은 2차원 배열이 오름차순으로 정렬되어야 한다.', () => {
-  const previousSortArray = [
-    [8, 21, 23, 41, 42, 43],
-    [3, 5, 11, 16, 32, 38],
-    [7, 11, 16, 35, 36, 44],
-    [1, 8, 11, 31, 41, 42],
-    [13, 14, 16, 38, 42, 45],
-  ];
+test('SortLotto: 정렬되지 않은 1차원 배열이 오름차순으로 정렬되어야 한다.', () => {
+  const previousSortArray = [8, 5, 23, 7, 42, 9];
 
   // 실행
   const afterSortArray = sortLotto(previousSortArray);

d5576db5d9db08b2547d473e081ede95cd7d4445
OH WOONG KWON <oungtom1212@naver.com>
2025-11-02 15:13:56 +0900

    feat: 로또 번호 출력 기능 구현
    로또 번호를 특정 형식으로 출력하는 메서드 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class Lotto {
   #numbers;
 
@@ -13,6 +15,11 @@ class Lotto {
   }
 
   // TODO: 추가 기능 구현
-}
 
+  // 생성자를 통해 설정된 로또 번호를 출력하는 함수
+  printLottoNumbers() {
+    // Console.print(this.#numbers);
+    Console.print(`[${this.#numbers.join(', ')}]`);
+  }
+}
 export default Lotto;

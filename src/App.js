dc271b4bcaf68cc78de4fbb383c6901921b8f968
OH WOONG KWON <oungtom1212@naver.com>
2025-11-02 15:58:14 +0900

    feat: 발행한 로또 구매 번호 출력
    Lotto.js에 구현된 Lotto 클래스를 App.js에서 사용하여 다음 기능들을 구현함
    
    - 사용자가 구매한 로또 번호 목록을 화면에 출력

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { Console } from '@woowacourse/mission-utils';
 import * as InputLotto from './InputLotto.js';
 import LottoGnerator from './LottoGnerator.js';
+import { CONSOLE_MESSAGE, LOTTO_CALCULATE_NUMBER } from './Constants.js';
 import sortLotto from './SortLotto.js';
+import Lotto from './Lotto.js';
 
 class App {
   async run() {
@@ -14,11 +16,27 @@ class App {
 
     const sortedLottoNumberArray = sortLotto(lottoNumberArray);
 
+    const lottoObjects = sortedLottoNumberArray.map(
+      (ticketNumbers) => new Lotto(ticketNumbers),
+    );
+
+    for (let i = 0; i < lottoObjects.length; i += 1) {
+      lottoObjects[i].printLottoNumbers();
+    }
+    Console.print('');
+
     const winningNumber = await InputLotto.getCorrectNumber();
     Console.print('');
 
     const bonusNumber = await InputLotto.getBonusNumber();
     Console.print('');
+
+    // 오름차순으로 몇등인지 배열에 담음
+    // ex: [0,0,0,3,5]
+    const lottoRankArray = lottoObjects.map((ele) =>
+      ele.showFinalResult(winningNumber, bonusNumber),
+    );
+    const sortedlottoRankArray = lottoRankArray.sort((a, b) => a - b);
   }
 }
 

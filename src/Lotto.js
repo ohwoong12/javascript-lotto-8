ca4c07db0de1f83230f02e472473000329a3f812
OH WOONG KWON <oungtom1212@naver.com>
2025-11-02 15:45:05 +0900

    feat: 로또 구매 번호와 당첨 번호를 비교하여 당첨된 번호의 개수를 계산 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -21,5 +21,18 @@ class Lotto {
     // Console.print(this.#numbers);
     Console.print(`[${this.#numbers.join(', ')}]`);
   }
+
+  /**
+   * 각 로또 1장마다 몇개의 번호가 당첨됐는지 비교하는 함수
+   * @param {number[]} correctNumbers - 로또 당첨 번호가 담긴 배열
+   * @returns {number} - 당첨된 번호의 개수
+   */
+  #compareOverlappingNumbers(correctNumbers) {
+    const matchCount = this.#numbers.filter((ele) =>
+      correctNumbers.includes(ele),
+    ).length;
+
+    return matchCount;
+  }
 }
 export default Lotto;

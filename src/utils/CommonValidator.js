95c31742e24571fe50c7c1c28d8f6ad951bc1caa
OH WOONG KWON <oungtom1212@naver.com>
2025-11-03 21:37:29 +0900

    feat: 공통 예외 처리 항목 기능 구현

---
+++ src/utils/CommonValidator.js
@@ -0,0 +1,31 @@
+import { ERROR_MESSAGE } from './Constants.js';
+
+/**
+ * 입력값이 숫자인지 검사하는 함수
+ * @param {string} value - 숫자 입력값
+ */
+export function validateTypeNumber(value) {
+  if (Number.isNaN(Number(value))) {
+    throw new Error(`${ERROR_MESSAGE.COMMON.TYPE}\n`);
+  }
+}
+
+/**
+ * 입력값이 정수인지 검사하는 함수
+ * @param {number} value - 숫자 입력값
+ */
+export function validateNumberIsInteger(value) {
+  if (!Number.isInteger(Number(value))) {
+    throw new Error(ERROR_MESSAGE.COMMON.TYPE);
+  }
+}
+
+/**
+ * 로또 번호 1개가 1~45 범위 내에 있는지 검사하는 함수
+ * @param {number} value - 로또 번호
+ */
+export function validateBonusNumberRange(bonusNumber) {
+  if (bonusNumber <= 0 || bonusNumber > 45) {
+    throw new Error(ERROR_MESSAGE.COMMON.RANGE);
+  }
+}

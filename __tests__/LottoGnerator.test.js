a7f53708b17731ef89c2b01722c1951b584e3a59
OH WOONG KWON <oungtom1212@naver.com>
2025-11-03 22:34:54 +0900

    test: LottoGnerator 테스트 코드 변경
    기존에 MVC 패턴 적용으로 인해 작동하지 않던 코드 변경

--- __tests__/LottoGnerator.test.js
+++ __tests__/LottoGnerator.test.js
@@ -1,5 +1,6 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import LottoGnerator from '../src/model/LottoGnerator.js';
+import Lotto from '../src/model/Lotto.js';
 
 const mockRandoms = (numbers) => {
   MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
@@ -13,21 +14,54 @@ describe('로또 생성 함수 테스트', () => {
     jest.restoreAllMocks();
   });
 
-  test('로또 생성 함수 테스트', () => {
+  test('구매 수량만큼 Lotto 객체 배열을 생성해야 한다', () => {
+    // 1. given
     const purchaseCount = 3;
-    const expectedLottos = [
+
+    const mockedNumbers = [
       [1, 2, 3, 4, 5, 6],
       [7, 8, 9, 10, 11, 12],
       [13, 14, 15, 16, 17, 18],
     ];
 
-    mockRandoms(expectedLottos);
+    const expectedLottos = [
+      new Lotto([1, 2, 3, 4, 5, 6]),
+      new Lotto([7, 8, 9, 10, 11, 12]),
+      new Lotto([13, 14, 15, 16, 17, 18]),
+    ];
 
+    mockRandoms(mockedNumbers);
+
+    // 2. when
     const result = LottoGnerator(purchaseCount);
 
+    // 3. then
     expect(MissionUtils.Random.pickUniqueNumbersInRange).toHaveBeenCalledTimes(
       purchaseCount,
     );
+    expect(result).toEqual(expectedLottos);
+  });
+
+  test('로또 번호가 정렬되어 생성되어야 한다', () => {
+    // 1. given
+    const purchaseCount = 2;
+
+    const mockedNumbers = [
+      [6, 5, 4, 3, 2, 1],
+      [12, 11, 10, 9, 8, 7],
+    ];
+
+    const expectedLottos = [
+      new Lotto([1, 2, 3, 4, 5, 6]),
+      new Lotto([7, 8, 9, 10, 11, 12]),
+    ];
+
+    mockRandoms(mockedNumbers);
+
+    // 2. when
+    const result = LottoGnerator(purchaseCount);
+
+    // 3. then
     expect(result).toEqual(expectedLottos);
   });
 });

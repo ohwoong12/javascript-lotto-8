06e285491ef67ae8d2f1cb007992c4069a0d084d
OH WOONG KWON <oungtom1212@naver.com>
2025-11-03 21:39:36 +0900

    feat: 로또 구매 금액 예외 처리 기능 구현

---
+++ src/utils/PurchaseCostValidator.js
@@ -0,0 +1,40 @@
+import { ERROR_MESSAGE } from './Constants.js';
+import {
+  validateTypeNumber,
+  validateNumberIsInteger,
+} from './CommonValidator.js';
+
+/**
+ * 로또 구매 금액이 양수인지 검사하는 함수
+ * @param {number} purchaseCost - 로또 구매 금액
+ */
+function validatePurchaseCostIsPositive(purchaseCost) {
+  if (purchaseCost <= 0) {
+    throw new Error(ERROR_MESSAGE.COMMON.TYPE);
+  }
+}
+
+/**
+ * 로또 구매 금액이 1,000원 단위인지 검사하는 함수
+ * @param {number} purchaseCost - 로또 구매 금액
+ */
+function validatePurchaseCostUnit(purchaseCost) {
+  if (purchaseCost % 1000 !== 0) {
+    throw new Error(ERROR_MESSAGE.PURCHASE.UNIT);
+  }
+}
+
+/**
+ * 로또 구매 금액에 대한 유효성 검사를 진행하는 함수
+ * - 숫자가 입력되지 않은 경우
+ * - 정수가 아닌 경우
+ * - 양수가 아닌 경우
+ * - 1,000 단위가 아닌 경우
+ * @param {number} purchaseCost - 로또 구매 금액
+ */
+export default function validatePurchaseCost(purchaseCost) {
+  validateTypeNumber(purchaseCost);
+  validateNumberIsInteger(purchaseCost);
+  validatePurchaseCostIsPositive(purchaseCost);
+  validatePurchaseCostUnit(purchaseCost);
+}

ce4d2413d0891d8ccbb408f05e42ec79b7523dbe
OH WOONG KWON <oungtom1212@naver.com>
2025-11-03 22:29:06 +0900

    feat: 단일 책임 원칙 적용을 위한 App 기능 분리
    App.js는 시작만 담당하고, 자세한 진행은 LottoController가 담당하도록 개편

--- src/controller/App.js
+++ src/controller/App.js
@@ -1,35 +1,9 @@
-import * as InputLotto from '../view/InputView.js';
-import LottoNumberGnerator from '../model/LottoGnerator.js';
-import { LOTTO_CALCULATE_NUMBER } from '../utils/Constants.js';
-import { calculatorFinalResults } from '../model/CalculateFinalResults.js';
-import {
-  printLottoTicket,
-  printProfitRate,
-  printPurchaseCount,
-  printStatistics,
-} from '../view/OutputView.js';
+import LottoController from './LottoController.js';
 
 class App {
   async run() {
-    const purchaseCost = await InputLotto.getLottoMoney();
-    const purchaseCount = purchaseCost / LOTTO_CALCULATE_NUMBER.PER_LOTTO_PRICE;
-    const lottoObjects = LottoNumberGnerator(purchaseCount);
-
-    printPurchaseCount(purchaseCount);
-    printLottoTicket(lottoObjects);
-
-    const winningNumber = await InputLotto.getCorrectNumber();
-    const bonusNumber = await InputLotto.getBonusNumber(winningNumber);
-
-    const finalResult = calculatorFinalResults(
-      lottoObjects,
-      winningNumber,
-      bonusNumber,
-      purchaseCost,
-    );
-
-    printStatistics(finalResult.statistics);
-    printProfitRate(finalResult.profitRate);
+    const lottoController = new LottoController();
+    await lottoController.start();
   }
 }
 
---
+++ src/controller/LottoController.js
@@ -0,0 +1,36 @@
+import * as InputLotto from '../view/InputView.js';
+import LottoNumberGnerator from '../model/LottoGnerator.js';
+import { LOTTO_CALCULATE_NUMBER } from '../utils/Constants.js';
+import { calculatorFinalResults } from '../model/CalculateFinalResults.js';
+import {
+  printLottoTicket,
+  printProfitRate,
+  printPurchaseCount,
+  printStatistics,
+} from '../view/OutputView.js';
+
+class LottoController {
+  async start() {
+    const purchaseCost = await InputLotto.getLottoMoney();
+    const purchaseCount = purchaseCost / LOTTO_CALCULATE_NUMBER.PER_LOTTO_PRICE;
+    const lottoObjects = LottoNumberGnerator(purchaseCount);
+
+    printPurchaseCount(purchaseCount);
+    printLottoTicket(lottoObjects);
+
+    const winningNumber = await InputLotto.getCorrectNumber();
+    const bonusNumber = await InputLotto.getBonusNumber(winningNumber);
+
+    const finalResult = calculatorFinalResults(
+      lottoObjects,
+      winningNumber,
+      bonusNumber,
+      purchaseCost,
+    );
+
+    printStatistics(finalResult.statistics);
+    printProfitRate(finalResult.profitRate);
+  }
+}
+
+export default LottoController;
